c81ad23a097c189178757b8cedb9bd34bc659878
George D'Souza <george@buddi.co.uk>
2021-03-31 06:48:03 +0100

    Updated the code base

---
+++ src/public/00.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="Rice Transplanter, rice seedlings, paddy field, Goa farming, Goan farmers, Rotary Tilling, Mechanised seeding of paddy in trays, Walk behind transplanter for smaller and difficult fields, Ride-on Rice transplanter for large fields, Weed control measures, Nursery management, Field preparation, farm production, farmer, farm management, farm technology, modern farming, soil sensor, crop production, pest management, soil management, precision farming, real-time sensor"/>
+  <meta name="description" content="Working with local Goan farmers to introduce mordern technology farming"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Pragma" content="no-cache" />
+  <meta http-equiv="Expires" content="0" />
+  <!-- Title -->
+  <title>Goencho Xetkar - Farm Services</title>
+  <!-- Favicon -->
+  <link rel="icon" href="img/core-img/favicon.ico">
+  <!-- Core Stylesheet -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body style="height:100%">
+	
+<div style="overflow:hidden; height:100%">
+	<table width="100%" cellpadding="0" cellspacing="0" style="height:100%" border="0">
+		<tr>
+			<td align="center"><img src="/img/core-img/logo.png" style="vertical-align:middle;" alt="Coming soon Goencho Xetkar" title="Goencho Xetkar" /></td>
+		</tr>
+		<tr>
+		<td align="center"><h1 class="display-2">COMING SOON</h1></td>
+		</tr>
+		<tr><td>&nbsp;</td></tr>
+		<tr>
+		<td align="center"><h3 class="display-4">to serve you locally</h3></td>
+		</tr>
+	</table>
+</div>
+</body>
+
+</html>
